e56f41d52ec4abf72c6843bb23da8eef07b6ba7c
Quangvn7 <quangvn7@viettel.com.vn>
2025-04-16 11:05:19 +0700

    feat: update package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FirebaseApp } from '@angular/fire/app';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
